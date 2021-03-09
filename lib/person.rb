6ecf1267a6e64f7b5cd95b8813675957202e8b30
Lrd134 <larryc3200@gmail.com>
2021-03-09 16:45:28 -0500

    commit tree 83e61a223e098a50cfda984ee294ae98a1589f93

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name=(d_name)
+        @name = d_name
+    end
+    def name
+        @name
+    end
+    def job=(p_job)
+        @job = p_job
+    end
+    def job
+        @job
+    end
+end
